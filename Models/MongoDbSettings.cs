55d231f42b804ae3c99b554730b8726c218e9b75
Shoriful Habib <shorifulhabib.iit@gmail.com>
2026-03-03 00:25:48 +0600

    mongodb added

---
+++ Models/MongoDbSettings.cs
@@ -0,0 +1,8 @@
+namespace vita_care.Models
+{
+    public class MongoDbSettings
+    {
+        public string ConnectionString { get; set; } = default!;
+        public string DatabaseName { get; set; } = default!;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
+using MongoDB.Driver;
+using vita_care.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Load .env file
+DotNetEnv.Env.Load();
+
 // Add services to the container.
+var mongoSettings = new MongoDbSettings
+{
+    ConnectionString = Environment.GetEnvironmentVariable("MONGODB_CONNECTION_STRING") ?? throw new InvalidOperationException("MONGODB_CONNECTION_STRING not found"),
+    DatabaseName = Environment.GetEnvironmentVariable("MONGODB_DATABASE_NAME") ?? throw new InvalidOperationException("MONGODB_DATABASE_NAME not found")
+};
+
+builder.Services.AddSingleton(mongoSettings);
+builder.Services.AddSingleton<IMongoClient>(new MongoClient(mongoSettings.ConnectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
